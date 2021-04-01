f66b02fdabbcc43544f87bc7a52a8cfc01f278ff
Daniel Silveira <danielsilveira.contato@gmail.com>
2021-04-01 14:13:47 -0300

    MasterClass Cod3r: Estudando os fundamentos do React

---
+++ src/components/Primeiro.jsx
@@ -0,0 +1,7 @@
+import React from 'react' // Sempre quando precisar renderizar jsx com tags html precisamos fazer esta importação
+
+export default () => 
+    <>
+        <h1>Primeiro Componente</h1>
+        <h2>Exemplo de um componente React</h2>
+    </>
--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './index.css'
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import ComParametro from './components/ComParametro'
+import Primeiro from './components/Primeiro'
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    <div>
+        <Primeiro/>
+        <ComParametro titulo="Esse é o título" subtitulo="Esse é o Subtítulo" />
+        <ComParametro titulo="Opa" subtitulo="Epa" />
+    </div>,
+    document.getElementById('root')
+)
